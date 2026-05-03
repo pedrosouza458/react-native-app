fa182dd8d2f873de541f9823f7ed7d9f18cbc78d
Pedro Souza <pedro.ads510@gmail.com>
2026-05-03 18:43:23 -0300

    style: adjust tabs padding

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,7 +14,6 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
   },
   title: {
     fontSize: 20,
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -14,7 +14,6 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
   },
   title: {
     fontSize: 20,
