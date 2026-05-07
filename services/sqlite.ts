0850edd5b14e5c6a8d52f921cff605d6b9710d99
Pedro Souza <pedro.ads510@gmail.com>
2026-05-06 21:50:35 -0300

    refactor: update sqlite schema to allow sync per user

--- services/sqlite.ts
+++ services/sqlite.ts
@@ -5,7 +5,7 @@ export async function initializeDatabase(db: SQLiteDatabase) {
     await db.execAsync(`
     PRAGMA journal_mode = WAL;
     CREATE TABLE IF NOT EXISTS users (
-     id TEXT NOT NULL,
+     id TEXT PRIMARY KEY NOT NULL,
      github_id INTEGER,
      name TEXT NOT NULL,
      email TEXT,
@@ -23,12 +23,12 @@ export async function initializeDatabase(db: SQLiteDatabase) {
      stargazers_count INTEGER NOT NULL DEFAULT 0,
      language TEXT,
      html_url TEXT NOT NULL,
-     deleted INTEGER NOT NULL DEFAULT 0,
-     synced INTEGER NOT NULL DEFAULT 0
     );
     CREATE TABLE IF NOT EXISTS user_favorite_repositories (
      user_id TEXT NOT NULL,
      repository_id INTEGER NOT NULL,
+     delete INTEGER NOT NULL DEFAULT 0,
+     synced INTEGER NOT NULL DEFAULT 0,
      PRIMARY KEY (user_id, repository_id),
      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
      FOREIGN KEY (repository_id) REFERENCES favorite_repositories(id) ON DELETE CASCADE
